e280cbf8b7db072a24e8e48032a09fbf2e350320
Yousaf Khan <khanyousaf184@gmail.com>
2018-12-08 01:01:14 +0500

    added RecyclerView to MainActivity

--- app/src/main/java/com/example/yousafkhan/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/yousafkhan/recyclerviewdemo/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.yousafkhan.recyclerviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize  and set up RecyclerView
+        recyclerView = findViewById(R.id.recyclerview);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
