78456028421f10b2c720c8c8a26ef87b0f05e2ae
Yousaf Khan <khanyousaf184@gmail.com>
2018-12-08 02:17:00 +0500

    added ItemKeyProvider for RecyclerView

---
+++ app/src/main/java/com/example/yousafkhan/recyclerviewdemo/recyclerview/MyItemKeyProvider.java
@@ -0,0 +1,31 @@
+package com.example.yousafkhan.recyclerviewdemo.recyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.example.yousafkhan.recyclerviewdemo.models.Contact;
+
+import java.util.List;
+
+import androidx.recyclerview.selection.ItemKeyProvider;
+
+public class MyItemKeyProvider extends ItemKeyProvider<Contact> {
+
+    private List<Contact> dataList;
+
+    protected MyItemKeyProvider(int scope, List<Contact> contactList) {
+        super(scope);
+        this.dataList = contactList;
+    }
+
+    @Nullable
+    @Override
+    public Contact getKey(int i) {
+        return dataList.get(i);
+    }
+
+    @Override
+    public int getPosition(@NonNull Contact contact) {
+        return dataList.indexOf(contact);
+    }
+}
